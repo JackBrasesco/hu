ae873abf00e5b03cfb936f1d1344ac0020480549
JackBrasesco <jacbras@nuevaschool.org>
2016-10-05 08:47:35 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var input = $("#new-item-input")
 input.keydown(function(e) {	
   if (e.keyCode == "13") {
     var valueToSave = input.val();
-    addListItem(valueToSave);
+    addListItem("jacksList",valueToSave);
   }
 })
 
